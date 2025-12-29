d7a4fe65311de5350927c65e67f9ca26d22419ce
Michael S. Collins <mscollins@barracuda.com>
2025-12-29 17:49:23 +0000

    Bump patch

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Application version
-VERSION = '1.4.1'
+VERSION = '1.4.2'
 
 class Config:
     """Base configuration"""
